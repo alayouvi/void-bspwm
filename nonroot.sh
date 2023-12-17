eef1e7653f410288e4d54b828c33253fed6257e5
alayouvi <153972865+alayouvi@users.noreply.github.com>
2023-12-17 18:36:22 +0700

    Add files via upload

---
+++ nonroot.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+
+cp -r $HOME/sys/git/void-bspwm/conf $HOME/
+cp $HOME/sys/git/void-bspwm/.xinitrc $HOME/
+cp $HOME/sys/git/void-bspwm/.bashrc $HOME/
+mv $HOME/conf $HOME/.config
+mkdir $HOME/usr
+mkdir $HOME/usr/art $HOME/usr/dump $HOME/usr/osint
+mkdir $HOME/sys/wall $HOME/sys/prog
+
