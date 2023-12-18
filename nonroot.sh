03e3786f6edce0f913ee91276d21f99fdca9e405
alayouvi <153972865+alayouvi@users.noreply.github.com>
2023-12-18 10:47:24 +0700

    nonroot.sh

--- nonroot.sh
+++ nonroot.sh
@@ -2,12 +2,15 @@
 #
 
 cp -r $HOME/sys/git/void-bspwm/conf $HOME/
+mv $HOME/conf $HOME/.config
 cp $HOME/sys/git/void-bspwm/.xinitrc $HOME/
 cp $HOME/sys/git/void-bspwm/.bashrc $HOME/
-mv $HOME/conf $HOME/.config
 mkdir $HOME/usr
 mkdir $HOME/usr/art $HOME/usr/dump $HOME/usr/osint
 mkdir $HOME/sys/wall $HOME/sys/prog
+chmod +x $HOME/.xinitrc
+chmod +x $HOME/.config/bspwm/bspwmrc
+cp $HOME/sys/git/wallpaper/* $HOME/sys/wall
 
 nix-channel --add http://nixos.org/channels/nixpkgs-unstable
 nix-channel --update
